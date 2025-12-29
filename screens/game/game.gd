aee2fd74a7eb866a0bf870386cefd22d5e1d0e1f
DinoStarBros <lmatthewalven@gmail.com>
2025-12-29 18:45:58 +0800

    Automatically restarts when both characters are not controlled by players
    Moved the restart logic to _process and adjusted it to differentiate between player and non-player control types. Also initialized someone_won in _ready and set control_type for P2 in the scene file.

--- screens/game/game.gd
+++ screens/game/game.gd
@@ -10,15 +10,12 @@ func _init() -> void:
 	# Signal connections
 	Global.win.connect(_win)
 
+func _ready() -> void:
+	someone_won = false
+
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene_to_file("res://screens/start/start.tscn")
-	
-	if (
-		Input.is_action_just_pressed("restart") and 
-		someone_won
-		):
-		get_tree().reload_current_scene()
 
 func _process(delta: float) -> void:
 	if Global.current_turn:
@@ -30,6 +27,22 @@ func _process(delta: float) -> void:
 	%arrow.global_position = %arrow.global_position.lerp(
 		arrow_desired_pos, 12 * delta
 	)
+
+	if (
+		Global.player_1.control_type == Global.player_1.control_types.PLAYER or
+		Global.player_2.control_type == Global.player_2.control_types.PLAYER
+		):
+		if (
+			Input.is_action_just_pressed("restart") and 
+			someone_won
+			):
+			get_tree().reload_current_scene()
+	else:
+		if (
+			someone_won
+			):
+			await get_tree().create_timer(0.1).timeout
+			get_tree().reload_current_scene()
 
 func _win(winning_char: Character) -> void:
 	print(winning_char.name, " Wins!")
