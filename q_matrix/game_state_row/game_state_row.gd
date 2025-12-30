b1dd4297a4e0cc6b5cf40d069fa3e709173d4cdc
DinoStarBros <lmatthewalven@gmail.com>
2025-12-30 15:08:23 +0800

    Add dynamic game state rows to QMatrix
    Introduced a reusable GameStateRow scene and script to represent game state rows. Updated QMatrix to dynamically instantiate 54 rows and refactored the scene structure to use a dedicated 'rows' container. Cleaned up the QMatrix scene by removing hardcoded row nodes and adjusted references accordingly.

---
+++ q_matrix/game_state_row/game_state_row.gd
@@ -0,0 +1,39 @@
+extends HBoxContainer
+class_name GameStateRow
+
+var index : int = 0
+
+var game_state : Array[int] = [
+	0, # HP (0 1 2) [0]
+	0, # Enemy HP (0 1 2) [1]
+	0, # Distance (0 1 2) [2]
+	0, # Energy (0 1) [3]
+]
+
+func _ready() -> void:
+	for n in index:
+		game_state[3] += 1
+		
+		if game_state[3] >= 2:
+			
+			game_state[3] = 0
+			game_state[2] += 1
+			
+			if game_state[2] >= 3:
+				
+				game_state[2] = 0
+				game_state[1] += 1
+				
+				if game_state[1] >= 3:
+					
+					game_state[1] = 0
+					game_state[0] += 1
+					
+
+	
+	%gs_txt.text = str(
+		game_state[0],
+		game_state[1],
+		game_state[2],
+		game_state[3],
+	)
--- q_matrix/q_matrix.gd
+++ q_matrix/q_matrix.gd
@@ -9,5 +9,17 @@ extends Control
 ## CONSEQUENTLY DESTROYING THE FUCKING PLANET
 class_name QMatrix
 
+@onready var rows: VBoxContainer = %rows
+
+var game_state_row_scn : PackedScene = preload("res://q_matrix/game_state_row/game_state_row.tscn")
+
 func _ready() -> void:
 	Global.q_matrix = self
+	
+	for n in 54:
+		_create_game_state_row(n)
+
+func _create_game_state_row(index: int) -> void:
+	var game_state_row : GameStateRow = game_state_row_scn.instantiate()
+	game_state_row.index = index
+	rows.add_child(game_state_row)
